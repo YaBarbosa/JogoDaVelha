a8f6434bb579f8248f8bff4a1f8901d85f81fd99
YaBarbosa <yasminbarbosa1800@gmail.com>
2024-10-16 16:17:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import './components/FirstComponent';
+import Board from './components/FirstComponent';
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+      
+      <Board/>
+    </div>
+      
+        
+    
+  )
+}
+
+export default App;
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,136 @@
+import { useState } from 'react';
+
+//definindo o quadrado do jogo
+function Square({ value, onSquareClick }) {
+  return (
+    <button id="tabuleiro"className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+/*o tabuleiro sendo efetuado
+useState = os parametros é como começa e o segundo irá ser alterado*/
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  //definindo um vencedor
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Vencedor: ' + winner;
+  } else {
+    status = 'Próximo jogador: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div id='jogo'>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  
+  //o codigo abaixo cria um array com 9 espaços
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  //definindo o histórico
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  //para o próximo movimento
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    let recomeco;
+    if (move > 0) {
+      description = 'Movimento  #' + move;
+    } 
+    
+    if(move<= 0){
+        description = 'Clique aqui para recomeçar '
+    }
+    
+
+    return (
+        /*chave de movimento dentro do jogo 
+        realiza uma lista que se atualiza de movimento*/
+      
+      
+      <li key={move}>
+        <button id ='historico' onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board id ="linha" xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+    //possiveis tipos de vitoria dentro do jogo
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game
